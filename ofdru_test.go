71bc146b13ba318deb1de72b78a0f94e36cd0968
Sergey Anntsupov <sergey@antsupov.xyz>
2020-03-17 13:31:08 +0300

    Update

--- ofdru_test.go
+++ ofdru_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestOfdRu(t *testing.T) {
-	client := OfdRu("3245001416", "mikhail.merkulov@megafon.ru", "121212", "https://ofd.ru")
+	client := OfdRu("3245001416", "mikhail.merkulov@megafon.ru", "121212", "https://demo.ofd.ru")
 	date := time.Date(2018, 04, 14, 0, 0, 0, 0, time.UTC)
 	rec, err := client.GetReceipts(date)
 	if err != nil {
